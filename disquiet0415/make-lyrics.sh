e883a6f6fa4d3e246cf48c880bb4ed1a33570b21
Paul Walker <paul@pwjw.com>
2019-12-13 13:53:25 -0500

    Make 3 voices of the tinsel

--- disquiet0415/make-lyrics.sh
+++ disquiet0415/make-lyrics.sh
@@ -1,6 +1,13 @@
-#curl -o 1.html https://www.lyrics.com/serp.php?st=tinsel\&p=1
-#curl -o 3.html https://www.lyrics.com/serp.php?st=tinsel\&p=2
-grep -i "<em>tinsel" ?.html > r1.txt
-perl -ne '$_ =~ s/<[^>]+>//g; print' r1.txt  > r2.txt
-perl -ne '$_ =~ s/^.+:\s*//g; print' r2.txt
+mkdir out
+mkdir tmp
+curl -o tmp/1.html https://www.lyrics.com/serp.php?st=tinsel\&p=1
+curl -o tmp/3.html https://www.lyrics.com/serp.php?st=tinsel\&p=2
+grep -i "<em>tinsel" tmp/?.html > tmp/r1.txt
+perl -ne '$_ =~ s/<[^>]+>//g; print' tmp/r1.txt  > tmp/r2.txt
+perl -ne '$_ =~ s/^.+:\s*//g; print' tmp/r2.txt > out/final-lyrics.txt
+say -v Samantha -o out/sam.aiff -f out/final-lyrics.txt 
+say -v Karen -o out/karen.aiff -f out/final-lyrics.txt 
+say -v Fiona -o out/fiona.aiff -f out/final-lyrics.txt 
+
+ 
 
