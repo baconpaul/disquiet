8ac98cbf49a57a66309ce4a626519579f994cc96
Paul Walker <paul@pwjw.com>
2019-12-15 12:58:12 -0500

    Wrapped up DQ415

--- disquiet0415/make-lyrics.sh
+++ disquiet0415/make-lyrics.sh
@@ -5,9 +5,8 @@ curl -o tmp/3.html https://www.lyrics.com/serp.php?st=tinsel\&p=2
 grep -i "<em>tinsel" tmp/?.html > tmp/r1.txt
 perl -ne '$_ =~ s/<[^>]+>//g; print' tmp/r1.txt  > tmp/r2.txt
 perl -ne '$_ =~ s/^.+:\s*//g; print' tmp/r2.txt > out/final-lyrics.txt
-say -v Samantha -o out/sam.aiff -f out/final-lyrics.txt 
-say -v Karen -o out/karen.aiff -f out/final-lyrics.txt 
 say -v Fiona -o out/fiona.aiff -f out/final-lyrics.txt 
+ffmpeg -i out/fiona.aiff out/fiona.wav
 
  
 
