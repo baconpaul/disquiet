822c128be3674f4d01bfb22b689411f44d6ee101
Paul Walker <paul@pwjw.com>
2020-01-10 13:10:05 -0500

    Getting ready to generate extra content

--- disquiet0419/convertOriginal.py
+++ disquiet0419/convertOriginal.py
@@ -4,3 +4,8 @@ for file in os.listdir( dir ):
     cmd = "sox \"{0}/{1}\" -b 16 -e signed-integer -r 48000 \"samples/{1}\"".format( dir, file );
     print( cmd );
     os.system(cmd);
+
+# then do this
+
+os.system( "rm samples/jet-jaguar* samples/zoundsabar*ooo\ ?.wav samples/zoundsabar*ooh\ ?.wav samples/Zero\ Meaning*" );
+
